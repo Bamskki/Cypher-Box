849dff49bc941f0c63af949c857c50746dd7935a
Sadiq Mustapha Aji <sadiqajimustapha@gmail.com>
2024-11-08 01:57:24 +0100

    fixed build script

--- scripts/build-release-apk.sh
+++ scripts/build-release-apk.sh
@@ -8,9 +8,9 @@
 #     $ xxd -plain -revert test.hex test2.txt
 
 
-echo $KEYSTORE_FILE_HEX > bluewallet-release-key.keystore.hex
-xxd -plain -revert bluewallet-release-key.keystore.hex > ./android/bluewallet-release-key.keystore
-rm bluewallet-release-key.keystore.hex
+echo $KEYSTORE_FILE_HEX > cypherbox-release-key.keystore.hex
+xxd -plain -revert cypherbox-release-key.keystore.hex > ./android/cypherbox-release-key.keystore
+rm cypherbox-release-key.keystore.hex
 
 cd android
 TIMESTAMP=$(date +%s)
@@ -19,5 +19,5 @@ sed -i'.original'  "s/versionCode 1/versionCode $TIMESTAMP/g" app/build.gradle
 mv ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk
 echo wheres waldo?
 find $ANDROID_HOME | grep apksigner | grep -v jar
-$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks ./bluewallet-release-key.keystore   --ks-pass=pass:$KEYSTORE_PASSWORD ./app/build/outputs/apk/release/app-release.apk
+$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks ./cypherbox-release-key.keystore   --ks-pass=pass:$KEYSTORE_PASSWORD ./app/build/outputs/apk/release/app-release.apk
 
